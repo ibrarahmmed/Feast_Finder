4932cd1996e76d6588f7730fb10ec876ac9a0bb8
ibrarahmmed <ibrarahmmedhstu@gmail.com>
2024-05-24 16:12:03 +0600

    add cataories

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../../components/Banner'
+import Catagories from './Catagories'
 
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <Catagories/>
     </div>
   )
 }
